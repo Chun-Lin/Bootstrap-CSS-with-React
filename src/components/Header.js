ab822adb22c8a293447572eba831d70916ca8b59
Chun-Lin <wulin40063@gmail.com>
2018-09-06 17:30:48 +0800

    feat: make navbar collapse can be showed when clicking toggler

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,60 @@
 import React from 'react'
+import classNames from 'classnames'
+class Header extends React.Component {
+  state = {
+    show: false,
+  }
 
-const Header = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div className="container-fluid">
-        <a href="#" className="navbar-brand h1">
-          Typeed
-        </a>
-        <button class="navbar-toggler" type="button">
-          <span class="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                Prize
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                Contact
-              </a>
-            </li>
-          </ul>
+  handleClick = event => {
+    this.setState(prevState => ({ show: !prevState.show }))
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div className="container-fluid">
+          <a href="#" className="navbar-brand h1">
+            Typeed
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            onClick={this.handleClick}
+          >
+            <span class="navbar-toggler-icon" />
+          </button>
+          <div
+            className={classNames('collapse', 'navbar-collapse', {
+              show: this.state.show,
+            })}
+          >
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  Prize
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  )
+      </nav>
+    )
+  }
 }
 
 export default Header
